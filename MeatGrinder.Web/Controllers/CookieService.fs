94160700ca48e495ac6e3911bd580f7c9334c107
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-07-15 00:45:41 -0400

    more work on conversion

--- MeatGrinder.Web/Controllers/CookieService.fs
+++ MeatGrinder.Web/Controllers/CookieService.fs
@@ -1,13 +1,22 @@
-﻿module MeatGrinder.Web.Services
+﻿namespace MeatGrinder.Web.Services
+open System
 open System.Web
 [<Sealed>]
 [<AbstractClass>]
 type CookieService =
     static member GetCookie (context:HttpContextBase) cookieName =
-        let httpCookie = context.Request.Cookies.["meatgrinder"]
+        let httpCookie = context.Request.Cookies.["MeatGrinder"]
         if httpCookie<>null then Some(httpCookie.[cookieName]) else None
-    static member SetCookie (response:HttpResponseBase, cookieName, cookieDuration, cookieValue) =
-        ()
+    static member SetCookie (response:HttpResponseBase, cookieName, cookieDuration:int, cookieValue) =
+        let cookie = new HttpCookie("MeatGrinder")
+        cookie.[cookieName] <- cookieValue
+        cookie.Expires <- DateTime.Now.AddDays (float(cookieDuration))
+        response.Cookies.Add(cookie)
     static member SetCookie (response:HttpResponseBase, cookieName, cookieDuration) =
         CookieService.SetCookie(response, cookieName, cookieDuration, "0")
-
+    static member DeleteCookie response cookieName =
+        CookieService.SetCookie(response, cookieName, -1)
+    static member GetUserId () =
+        let context = System.Web.HttpContextWrapper System.Web.HttpContext.Current
+        let cookie = CookieService.GetCookie context "UserId"
+        if cookie.IsSome && cookie.Value<> null then Some(Int32.Parse(cookie.Value)) else None
