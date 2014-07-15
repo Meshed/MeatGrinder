89f584725c79ff37fcd191a3e9a321f10e930f58
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-07-14 23:58:26 -0400

    repo clean and branch for linq to sql DAL

---
+++ MeatGrinder.Dal2/DbContext.fs
@@ -0,0 +1,3 @@
+﻿module DbContext
+open Microsoft.FSharp.Data.TypeProviders
+type MeatGrinderContext = SqlDataConnection<""" Server=tcp:ywckntoq5w.database.windows.net,1433;Database=MeatGrinder;User ID=meat_grinder@ywckntoq5w;Password=Shoestrings2;MultipleActiveResultSets=True;Encrypt=True;Connection Timeout=30 """>
