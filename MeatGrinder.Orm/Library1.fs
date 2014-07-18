30f79946b0e79c56ca0ae68b77d074c2a8a58f6d
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-07-17 23:15:18 -0400

    starting a code first F# orm

---
+++ MeatGrinder.Orm/Library1.fs
@@ -0,0 +1,17 @@
+﻿namespace MeatGrinder.Orm
+open System
+open System.Data.Entity
+open System.Data.Entity.Infrastructure
+open System.Data.Objects
+open System.Data.Objects.DataClasses
+open System.Linq
+type Goal = {Id:int}
+
+type MeatGrinderEntities() = 
+    inherit DbContext("name=MeatGrinderEntities")
+//    override x.OnModelCreating (modelBuilder:DbModelBuilder) =
+//        ()
+    let mutable _goals:DbSet<Goal> = null
+    member public x.Goals 
+        with get() = _goals
+        and  set v = _goals <- v
